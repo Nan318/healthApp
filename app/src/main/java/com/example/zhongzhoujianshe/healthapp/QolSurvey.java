d6eb6ba18832ee614325187ff37b3f32b622a6f4
Nan318 <nzhang5@student.unimelb.edu.au>
2018-10-01 01:23:37 +1000

    qol单选：大致排版；         七选一获得radio button checkedid；         四选一实现单选 并获得checkedid；         菜单栏：布局：back，title，send；                对话框跳出 缺：    单选按钮略大；    选中效果：文字背景颜色；    问题：两端对齐？；    菜单栏：back的save，delete功能           send：对话框关闭后跳到未答的题处？

--- app/src/main/java/com/example/zhongzhoujianshe/healthapp/QolSurvey.java
+++ app/src/main/java/com/example/zhongzhoujianshe/healthapp/QolSurvey.java
@@ -1,25 +1,24 @@
 package com.example.zhongzhoujianshe.healthapp;
 
-import android.support.v4.view.PagerAdapter;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class QolSurvey extends AppCompatActivity implements
         ViewPager.OnPageChangeListener {
     //UI Objects
+    private AlertDialog alert = null;
+    private AlertDialog.Builder builder = null;
     private ViewPager viewPager;
-
     private QolSurveyFragmentPagerAdapter qolSurveyAdapter;
 
     //几个代表页面的常量
@@ -31,15 +30,73 @@ public class QolSurvey extends AppCompatActivity implements
     public static final int PAGE_FIVE = 4;
     public static final int PAGE_SIX = 5;
 
+    //private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qol__survey);
+
+        //used for setting icon-font
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/iconfont.ttf");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.qolSurveyToolbar);
+        // Title
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        //set icon-font: send
+        TextView txt_menu_send = (TextView) toolbar.findViewById(R.id.toolbar_send);
+        txt_menu_send.setTypeface(font);
+        //set click event
+        txt_menu_send.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                showSendDialog();
+            }
+        });
+        //back text_btn
+        TextView txt_menu_back = (TextView) toolbar.findViewById(R.id.toolbar_back);
+        txt_menu_back.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                showBackDialog();
+            }
+        });
+
         qolSurveyAdapter = new QolSurveyFragmentPagerAdapter(getSupportFragmentManager());
         bindViews();
     }
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.survey_menu, menu);
+        MenuItem item = menu.findItem(R.id.menu_send);
+        //getActionView()返回你自定义的菜单布局，设置单击事件的目的是，让其单击时执行onOptionsItemSelected，从而只需统一在onOptionsItemSelected处理即可
 
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/iconfont.ttf");//记得加上这句
+         //   mTitle.setTypeface(font);
+      //  TextView menusend = (TextView) menu.findItem(R.id.menu_send);
+        item.setTypeface(font);
+
+        return true;
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.menu_send:
+                Toast.makeText(this , "haha" , Toast.LENGTH_SHORT).show();
+
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+*/
     private void bindViews() {
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
@@ -82,5 +139,65 @@ public class QolSurvey extends AppCompatActivity implements
                     break;
             }
         }
+    }
+    private void showBackDialog() {
+        //加载布局并初始化组件
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.qol_survey_threebtn_dialog,null);
+        Button dialogBtnSave = (Button) dialogView.findViewById(R.id.btn_save);
+        Button dialogBtnDelete = (Button) dialogView.findViewById(R.id.btn_delete);
+        Button dialogBtnCancel = (Button) dialogView.findViewById(R.id.btn_cancel);
+
+        builder = new AlertDialog.Builder(this);
+        //builder.setTitle(getString(R.string.icon_warning));
+        builder.setView(dialogView);
+        //点击对话框以外的区域是否让对话框消失
+        builder.setCancelable(false);
+        alert = builder.create();
+        alert.show();
+
+        //设置组件
+        dialogBtnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QolSurvey.this,"sendBtn！！",Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialogBtnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QolSurvey.this,"deleteBtn！！",Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialogBtnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "对话框已关闭~", Toast.LENGTH_SHORT).show();
+                alert.dismiss();
+            }
+        });
+
+
+    }
+    private void showSendDialog() {
+        //加载布局并初始化组件
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.qol_survey_onebtn_dialog,null);
+        Button dialogBtnOk = (Button) dialogView.findViewById(R.id.btn_ok);
+
+        builder = new AlertDialog.Builder(this);
+        //builder.setTitle(getString(R.string.icon_warning));
+        builder.setView(dialogView);
+        //点击对话框以外的区域是否让对话框消失
+        builder.setCancelable(false);
+        alert = builder.create();
+        alert.show();
+
+        //设置组件
+        dialogBtnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "对话框已关闭~", Toast.LENGTH_SHORT).show();
+                alert.dismiss();
+            }
+        });
     }
 }
